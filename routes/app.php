0d073e8d25b8e6fcd6511f6a8416e5c121986434
Mylena Klepka <gomes.klepka98@gmail.com>
2024-10-29 21:30:19 -0300

    correçoes no arquivo

--- routes/app.php
+++ routes/app.php
@@ -1,10 +1,13 @@
 <?php
 
 use MaisAutonomia\Controllers\App\AppController;
+use MaisAutonomia\Controllers\App\ServiceController;
 
 $app->get('/me/inicio', [AppController::class, 'home']);
 $app->get('/me/perfil', [AppController::class, 'profile']);
 $app->get('/me/menssagens', [AppController::class, 'messages']);
+
 $app->get('/me/servicos/{id}', [AppController::class, 'details']);
+$app->post('/me/servicos/cadastro', [ServiceController::class, 'store']);
 
 $app->get('/me/deletar/usuario', [AppController::class, 'delete']);
